099e6023d70de68add490a2e240ad7f45dc326a5
mati <matiasvillaverde@protonmail.com>
2023-04-28 14:16:16 +0200

    refactor: remove target from git

--- target/debug/deps/trust_core-3bef8b818c9944f1.d
+++
@@ -1,5 +0,0 @@
-/Users/mati/Code/trust/target/debug/deps/trust_core-3bef8b818c9944f1: trust-core/src/lib.rs
-
-/Users/mati/Code/trust/target/debug/deps/trust_core-3bef8b818c9944f1.d: trust-core/src/lib.rs
-
-trust-core/src/lib.rs:
