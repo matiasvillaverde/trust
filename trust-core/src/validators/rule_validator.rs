4796c9acd421aaa2ae47f86d4bcc472a305f3cd2
mati <matiasvillaverde@protonmail.com>
2023-05-23 18:08:01 +0200

    feat: Improve trade validation with decimal libraries and error handling
    - Integrate `rust_decimal` and `rust_decimal_macros` libraries
    - Implement `validate_trade` function with fund and risk checks
    - Sort rules by priority before name matching in `validate_trade`
    - Improve error handling for missing account overview during validation

--- trust-core/src/validators/rule_validator.rs
+++ trust-core/src/validators/rule_validator.rs
@@ -1,5 +1,8 @@
+use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
 use std::error::Error;
 use trust_model::{Account, Database, RuleName, Trade};
+
 pub struct RuleValidator;
 type RuleValidationResult = Result<(), Box<RuleValidationError>>;
 
@@ -23,13 +26,75 @@ impl RuleValidator {
     }
 
     pub fn validate_trade(trade: &Trade, database: &mut dyn Database) -> RuleValidationResult {
-        unimplemented!()
+        let overview = database.read_account_overview_currency(trade.account_id, &trade.currency);
+        let overview = match overview {
+            Ok(overview) => overview,
+            Err(error) => {
+                return Err(Box::new(RuleValidationError {
+                    code: RuleValidationErrorCode::NotEnoughFunds,
+                    message: format!(
+                        "Not enough funds in account {} for currency {} with error: {}",
+                        trade.account_id, trade.currency, error
+                    ),
+                }))
+            }
+        };
+        let available = overview.total_available.amount;
+
+        if available < (trade.entry.unit_price.amount * Decimal::from(trade.entry.quantity)) {
+            return Err(Box::new(RuleValidationError {
+                code: RuleValidationErrorCode::NotEnoughFunds,
+                message: format!(
+                    "Not enough funds in account {} for currency {}. Available: {} and you are trying to trade: {}",
+                    trade.account_id, trade.currency, available, trade.entry.unit_price.amount * Decimal::from(trade.entry.quantity)
+                ),
+            }));
+        }
+
+        // Get rules by priority
+        let mut rules = database
+            .read_all_rules(trade.account_id)
+            .unwrap_or_else(|_| vec![]);
+        rules.sort_by(|a, b| a.priority.cmp(&b.priority));
+
+        // match rules by name
+        for rule in rules {
+            match rule.name {
+                RuleName::RiskPerMonth(_risk) => {
+                    unimplemented!("risk_per_month")
+                }
+                RuleName::RiskPerTrade(risk) => {
+                    let risk_per_trade =
+                        trade.entry.unit_price.amount - trade.safety_stop.unit_price.amount;
+                    let total_risk = risk_per_trade * Decimal::from(trade.entry.quantity);
+                    let maximum_risk =
+                        available * (Decimal::from_f32_retain(risk).unwrap() / dec!(100.0));
+
+                    if total_risk > maximum_risk {
+                        return Err(Box::new(RuleValidationError {
+                            code: RuleValidationErrorCode::RiskPerTradeExceeded,
+                            message: format!(
+                                "Risk per trade exceeded for rule {}, maximum that can be at risk is {}, trade is attempting to risk {}",
+                                rule.name,
+                                maximum_risk,
+                                total_risk,
+                            ),
+                        }));
+                    }
+                }
+            }
+        }
+
+        // If no rule is violated, return Ok
+        Ok(())
     }
 }
 
 #[derive(Debug, PartialEq)]
 pub enum RuleValidationErrorCode {
     RuleAlreadyExistsInAccount,
+    RiskPerTradeExceeded,
+    NotEnoughFunds,
 }
 
 #[derive(Debug)]
