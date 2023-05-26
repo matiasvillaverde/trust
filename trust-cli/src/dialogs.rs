ba0b300b2de0849fe58c139bdcf7386744b6d41e
mati <matiasvillaverde@protonmail.com>
2023-05-26 14:57:18 +0200

    feat: add an exit dialog for manually execution of targets and stops

--- trust-cli/src/dialogs.rs
+++ trust-cli/src/dialogs.rs
@@ -1,5 +1,6 @@
 mod account_dialog;
 mod entry_dialog;
+mod exit_dialog;
 mod rule_dialog;
 mod trade_approver_dialog;
 mod trade_dialog;
@@ -9,6 +10,7 @@ mod transaction_dialog;
 pub use account_dialog::AccountDialogBuilder;
 pub use account_dialog::AccountSearchDialog;
 pub use entry_dialog::EntryDialogBuilder;
+pub use exit_dialog::ExitDialogBuilder;
 pub use rule_dialog::RuleDialogBuilder;
 pub use rule_dialog::RuleRemoveDialogBuilder;
 pub use trade_approver_dialog::TradeDialogApproverBuilder;
---
+++ trust-cli/src/dialogs/exit_dialog.rs
@@ -0,0 +1,91 @@
+use crate::dialogs::AccountSearchDialog;
+use crate::views::{AccountOverviewView, TradeOverviewView, TradeView, TransactionView};
+use dialoguer::{theme::ColorfulTheme, FuzzySelect};
+use std::error::Error;
+use trust_core::Trust;
+use trust_model::{Account, AccountOverview, Trade, Transaction};
+
+type ExitDialogBuilderResult = Option<Result<(Transaction, AccountOverview), Box<dyn Error>>>;
+
+pub struct ExitDialogBuilder {
+    account: Option<Account>,
+    trade: Option<Trade>,
+    result: ExitDialogBuilderResult,
+}
+
+impl ExitDialogBuilder {
+    pub fn new() -> Self {
+        ExitDialogBuilder {
+            account: None,
+            trade: None,
+            result: None,
+        }
+    }
+
+    pub fn record_stop(mut self, trust: &mut Trust) -> ExitDialogBuilder {
+        let trade: Trade = self.trade.clone().unwrap();
+        self.result = Some(trust.record_stop(&trade));
+        self
+    }
+
+    pub fn record_target(mut self, trust: &mut Trust) -> ExitDialogBuilder {
+        let trade: Trade = self.trade.clone().unwrap();
+        self.result = Some(trust.record_target(&trade));
+        self
+    }
+
+    pub fn display(self) {
+        match self
+            .result
+            .expect("No result found, did you forget to call search?")
+        {
+            Ok((tx, overview)) => {
+                let account_name = self.account.clone().unwrap().name;
+
+                println!("Trade exit executed:");
+                TradeView::display_trade(&self.trade.unwrap(), account_name.as_str());
+
+                println!("With transaction:");
+                TransactionView::display(&tx, account_name.as_str());
+
+                println!("Account overview:");
+                AccountOverviewView::display(overview, account_name.as_str());
+            }
+            Err(error) => println!("Error approving trade: {:?}", error),
+        }
+    }
+
+    pub fn account(mut self, trust: &mut Trust) -> Self {
+        let account = AccountSearchDialog::new().search(trust).build();
+        match account {
+            Ok(account) => self.account = Some(account),
+            Err(error) => println!("Error searching account: {:?}", error),
+        }
+        self
+    }
+
+    pub fn search(mut self, trust: &mut Trust) -> Self {
+        let trades =
+            trust.search_all_approved_trades_waiting_for_entry(self.account.clone().unwrap().id); // TODO: search trades that are already in.
+        match trades {
+            Ok(trades) => {
+                if trades.is_empty() {
+                    panic!("No trade found, did you forget to create one?")
+                }
+                let trade = FuzzySelect::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Trade:")
+                    .items(&trades[..])
+                    .default(0)
+                    .interact_opt()
+                    .unwrap()
+                    .map(|index| trades.get(index).unwrap())
+                    .unwrap();
+
+                self.trade = Some(trade.to_owned());
+            }
+            Err(error) => self.result = Some(Err(error)),
+        }
+
+        self
+    }
+}
