e9e111c0c8f71e6f632a8f26278911716223a279
mati <matiasvillaverde@protonmail.com>
2023-06-01 15:04:46 +0200

    refactor: Refactor trade calculator to use UUIDs for database integration.
    Refactor trade calculator performance module for improved database usage
    - Use `Uuid` instead of trade reference in `performance.rs`
    - Add `Uuid` to `TradePerformance` method for database usage
    - Implement `MockDatabase` tests for `TradePerformance`

--- trust-core/src/trade_calculators/performance.rs
+++ trust-core/src/trade_calculators/performance.rs
@@ -1,17 +1,18 @@
 use rust_decimal::Decimal;
 use rust_decimal_macros::dec;
-use trust_model::{ReadTransactionDB, Trade, TransactionCategory};
+use trust_model::{ReadTransactionDB, TransactionCategory};
+use uuid::Uuid;
 
 pub struct TradePerformance;
 
 impl TradePerformance {
     pub fn calculate(
-        trade: &Trade,
+        trade_id: Uuid,
         database: &mut dyn ReadTransactionDB,
     ) -> Result<Decimal, Box<dyn std::error::Error>> {
         let mut performance = dec!(0);
 
-        for tx in database.all_trade_transactions(trade.id)? {
+        for tx in database.all_trade_transactions(trade_id)? {
             match tx.category {
                 TransactionCategory::OpenTrade(_)
                 | TransactionCategory::FeeClose(_)
@@ -26,5 +27,92 @@ impl TradePerformance {
         }
 
         Ok(performance)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::mocks::read_transaction_db_mocks::MockDatabase;
+    use rust_decimal_macros::dec;
+
+    #[test]
+    fn test_calculate_with_empty_transactions() {
+        let trade_id = Uuid::new_v4();
+        let mut database = MockDatabase::new();
+
+        let result = TradePerformance::calculate(trade_id, &mut database);
+        assert_eq!(result.unwrap(), dec!(0));
+    }
+
+    #[test]
+    fn test_calculate_with_transactions_to_ignore() {
+        let mut database = MockDatabase::new();
+
+        // One deposit transaction in the database
+        database.set_transaction(TransactionCategory::Deposit, dec!(100));
+        database.set_transaction(TransactionCategory::Withdrawal, dec!(100));
+        database.set_transaction(TransactionCategory::FundTrade(Uuid::new_v4()), dec!(100));
+        database.set_transaction(
+            TransactionCategory::PaymentEarnings(Uuid::new_v4()),
+            dec!(100),
+        );
+        database.set_transaction(TransactionCategory::WithdrawalEarnings, dec!(100));
+
+        let result = TradePerformance::calculate(Uuid::new_v4(), &mut database);
+        assert_eq!(result.unwrap(), dec!(0));
+    }
+
+    #[test]
+    fn test_calculate_with_transactions_hit_target() {
+        let mut database = MockDatabase::new();
+
+        database.set_transaction(TransactionCategory::FundTrade(Uuid::new_v4()), dec!(100));
+        database.set_transaction(TransactionCategory::OpenTrade(Uuid::new_v4()), dec!(100));
+        database.set_transaction(TransactionCategory::FeeClose(Uuid::new_v4()), dec!(1));
+        database.set_transaction(TransactionCategory::FeeOpen(Uuid::new_v4()), dec!(1));
+        database.set_transaction(TransactionCategory::PaymentTax(Uuid::new_v4()), dec!(20));
+        database.set_transaction(TransactionCategory::CloseTarget(Uuid::new_v4()), dec!(200));
+
+        let result = TradePerformance::calculate(Uuid::new_v4(), &mut database);
+        assert_eq!(result.unwrap(), dec!(78));
+    }
+
+    #[test]
+    fn test_calculate_with_transactions_hit_safety_stop() {
+        let trade_id = Uuid::new_v4();
+        let mut database = MockDatabase::new();
+
+        database.set_transaction(TransactionCategory::FundTrade(Uuid::new_v4()), dec!(100));
+        database.set_transaction(TransactionCategory::OpenTrade(Uuid::new_v4()), dec!(100));
+        database.set_transaction(TransactionCategory::FeeClose(Uuid::new_v4()), dec!(1));
+        database.set_transaction(TransactionCategory::FeeOpen(Uuid::new_v4()), dec!(1));
+        database.set_transaction(TransactionCategory::PaymentTax(Uuid::new_v4()), dec!(0));
+        database.set_transaction(
+            TransactionCategory::CloseSafetyStop(Uuid::new_v4()),
+            dec!(80),
+        );
+
+        let result = TradePerformance::calculate(trade_id, &mut database);
+        assert_eq!(result.unwrap(), dec!(-22));
+    }
+
+    #[test]
+    fn test_calculate_with_transactions_hit_safety_stop_slippage() {
+        let trade_id = Uuid::new_v4();
+        let mut database = MockDatabase::new();
+
+        database.set_transaction(TransactionCategory::FundTrade(Uuid::new_v4()), dec!(100));
+        database.set_transaction(TransactionCategory::OpenTrade(Uuid::new_v4()), dec!(100));
+        database.set_transaction(TransactionCategory::FeeClose(Uuid::new_v4()), dec!(1));
+        database.set_transaction(TransactionCategory::FeeOpen(Uuid::new_v4()), dec!(1));
+        database.set_transaction(TransactionCategory::PaymentTax(Uuid::new_v4()), dec!(0));
+        database.set_transaction(
+            TransactionCategory::CloseSafetyStopSlippage(Uuid::new_v4()),
+            dec!(50),
+        );
+
+        let result = TradePerformance::calculate(trade_id, &mut database);
+        assert_eq!(result.unwrap(), dec!(-52));
     }
 }
